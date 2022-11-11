f84c506cffde48b50e38a28b92a82931011a0443
Tooboi <japica123@gmail.com>
2022-11-10 22:02:29 -0500

    var declaration, text logic, starting to work out main quiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,64 @@
 // THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and score
+
+
+//variable declaration
+var startQuiz = document.querySelector("#buttStart");
+var buttonA = document.querySelector("#buttA");
+var buttonB = document.querySelector("#buttB");
+var buttonC = document.querySelector("#buttC");
+var buttonD = document.querySelector("#buttD");
+var h1Question = document.querySelector("#questionVisible");
+var timerUser = document.querySelector("#timerVisible")
+var rulesText = document.querySelector("#ruleText")
+rulesText.setAttribute("style", "display: block;");
+timerUser.textContent = " ";
+h1Question.textContent = "Press start to test your web dev knowledge";
+startQuiz.setAttribute("style", "margin: 0 auto; min-width: 20rem");
+buttonA.setAttribute("style", "display: none;");
+buttonB.setAttribute("style", "display: none;");
+buttonC.setAttribute("style", "display: none;");
+buttonD.setAttribute("style", "display: none;");
+var timeLeft = 100;
+var score = {
+    playerName: [],
+    playerScore: []
+};
+var questions = {
+    asking: [],
+    choiceA: [],
+    choiceB: [],
+    choiceC: [],
+    choiceD: [],
+    answerCorrect: []
+};
+
+
+//start the timer
+function runTimer() {
+    var interval = setInterval(function() {
+        // timeRemaining--;
+        timerUser.textContent = "Time: " + timeLeft;
+    })
+}
+
+
+
+//run the quiz
+function runQuiz() {
+    runTimer();
+    console.log("runQuiz has run");
+    buttonA.setAttribute("style", "display: block;");
+    buttonB.setAttribute("style", "display: block;");
+    buttonC.setAttribute("style", "display: block;");
+    buttonD.setAttribute("style", "display: block;");
+    rulesText.setAttribute("style", "display: none;");
+    h1Question.textContent = questions.asking[i];
+}
+// click start button to start game
+startQuiz.addEventListener("click", function() {
+    console.log("click");
+    startQuiz.setAttribute("style", "display: none;");
+    runQuiz();
+});
